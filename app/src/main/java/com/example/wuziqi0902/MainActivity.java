f9d36a8166131915869347bbcaf40fcb5e83f370
Famiglistim-m <139785499+Famiglistim-m@users.noreply.github.com>
2024-09-10 09:18:47 +0800

    底部菜单栏

--- app/src/main/java/com/example/wuziqi0902/MainActivity.java
+++ app/src/main/java/com/example/wuziqi0902/MainActivity.java
@@ -1,14 +1,138 @@
 package com.example.wuziqi0902;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private RadioGroup mRadioGroup;
+    private RadioButton tab1,tab2,tab3,tab4;  //四个单选按钮
+    private List<View> mViews;   //存放视图
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();//初始化数据
+        //对单选按钮进行监听，选中、未选中
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.rb_play:
+                        mViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.rb_chat:
+                        mViewPager.setCurrentItem(1);
+                        break;
+                    case R.id.rb_online:
+                        mViewPager.setCurrentItem(2);
+                        break;
+                    case R.id.rb_my:
+                        mViewPager.setCurrentItem(3);
+                        break;
+                }
+            }
+        });
+
+    }
+
+    private void initView() {
+        //初始化控件
+        mViewPager=findViewById(R.id.viewpager);
+        mRadioGroup=findViewById(R.id.rg_tab);
+        tab1=findViewById(R.id.rb_play);
+        tab2=findViewById(R.id.rb_chat);
+        tab3=findViewById(R.id.rb_online);
+        tab4=findViewById(R.id.rb_my);
+
+        mViews=new ArrayList<View>();//加载，添加视图
+        mViews.add(LayoutInflater.from(this).inflate(R.layout.play,null));
+        mViews.add(LayoutInflater.from(this).inflate(R.layout.chat,null));
+        mViews.add(LayoutInflater.from(this).inflate(R.layout.online,null));
+        mViews.add(LayoutInflater.from(this).inflate(R.layout.my,null));
+
+        mViewPager.setAdapter(new MyViewPagerAdapter());//设置一个适配器
+        //对viewpager监听，让分页和底部图标保持一起滑动
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override   //让viewpager滑动的时候，下面的图标跟着变动
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        tab1.setChecked(true);
+                        tab2.setChecked(false);
+                        tab3.setChecked(false);
+                        tab4.setChecked(false);
+                        break;
+                    case 1:
+                        tab1.setChecked(false);
+                        tab2.setChecked(true);
+                        tab3.setChecked(false);
+                        tab4.setChecked(false);
+                        break;
+                    case 2:
+                        tab1.setChecked(false);
+                        tab2.setChecked(false);
+                        tab3.setChecked(true);
+                        tab4.setChecked(false);
+                        break;
+                    case 3:
+                        tab1.setChecked(false);
+                        tab2.setChecked(false);
+                        tab3.setChecked(false);
+                        tab4.setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    //ViewPager适配器
+    private class MyViewPagerAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return mViews.size();
+        }
+
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view==object;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView(mViews.get(position));
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            container.addView(mViews.get(position));
+            return mViews.get(position);
+        }
     }
 }
